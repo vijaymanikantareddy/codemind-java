f5f6ca96ab6af89a3212a070e544b18dbed34533
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-08-20 21:46:16 +0530

    import java.util.Scanner; class Sample{     public static boolean prime(int n){         if(n<=1) return false;         for(int i=2 ; i*i<=n ; i++){             if(n%i==0)                 return false;         }         return true;     }     public static void main(String args[]){         Scanner s = new Scanner(System.in);         int n=s.nextInt();         int arr[]= new int[n];         for(int i=0 ; i<n ; i++){             arr[i]=s.nextInt();         }         int a = s.nextInt();         int cnt=0;         for(int i: arr){             if(i>=a && prime(i)){                 cnt++;             }         }         System.out.println(cnt);     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Sample{
+    public static boolean prime(int n){
+        if(n<=1) return false;
+        for(int i=2 ; i*i<=n ; i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]= new int[n];
+        for(int i=0 ; i<n ; i++){
+            arr[i]=s.nextInt();
+        }
+        int a = s.nextInt();
+        int cnt=0;
+        for(int i: arr){
+            if(i>=a && prime(i)){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
