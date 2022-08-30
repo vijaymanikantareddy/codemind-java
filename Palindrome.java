24859ff4c04d0a24f331e906cf1c9a0722d13df2
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-08-30 18:32:49 +0530

    import java.util.Scanner; class Sample {     public static boolean ispalin(int n){         int res=0, temp=n;         while(n>0){             res = res*10+(n%10);             n/=10;         }         return temp==res;     }     public static void main(String args[])     {         Scanner s = new Scanner(System.in);         int a = s.nextInt();         if(ispalin(a)){             System.out.println("True");         }         else{             System.out.println("False");         }     } }

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Sample
+{
+    public static boolean ispalin(int n){
+        int res=0, temp=n;
+        while(n>0){
+            res = res*10+(n%10);
+            n/=10;
+        }
+        return temp==res;
+    }
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        if(ispalin(a)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
