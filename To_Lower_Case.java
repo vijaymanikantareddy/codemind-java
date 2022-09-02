4c5b81aa67a0d66b7d8f2cf752eb062af4d94dda
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-09-02 07:43:33 +0530

    import java.util.*; class Sample{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         String st = sc.nextLine();         System.out.println(st.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Sample{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st.toLowerCase());
+    }
+}
