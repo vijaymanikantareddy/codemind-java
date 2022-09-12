f34f0a0e6fd8d1dca427fd860c9cc2608ad8dcfa
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-09-12 12:10:19 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i=1 ; i<=n ; i++)         {             for(int j=1 ; j<=n ; j++){                 if(j==1){                     System.out.print("* ");                 }                 else if(j==n){                     System.out.print("* ");                 }                 else if(i==j){                     System.out.print("* ");                 }                 else                     System.out.print("  ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
-class Pattern
+class Sample
 {
     public static void main(String args[])
     {
-        Scanner s = new Scanner(System.in);
-        int n=s.nextInt();
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=n-2 ; j++){
-                System.out.print(j);
-            }
-            for(int k=n-2 ; k>=1 ; k--){
-                System.out.print(k);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1 ; i<=n ; i++)
+        {
+            for(int j=1 ; j<=n ; j++){
+                if(j==1){
+                    System.out.print("* ");
+                }
+                else if(j==n){
+                    System.out.print("* ");
+                }
+                else if(i==j){
+                    System.out.print("* ");
+                }
+                else 
+                    System.out.print("  ");
             }
             System.out.println();
-
         }
     }
 }
