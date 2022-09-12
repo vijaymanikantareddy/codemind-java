0a8488241f54b36be236ba474ad868e9c0298a59
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-09-12 17:39:34 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i=1 ; i<=n ; i++)         {             if(i==n){                 for(int j=1 ; j<=n ; j++){                     System.out.print("*");                 }             }             else{                 for(int j=1 ; j<=n ; j++){                     if(j==1 || j==i)                         System.out.print("*");                     else                         System.out.print(" ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
-class Pattern
+class Sample
 {
     public static void main(String args[])
     {
-        Scanner s = new Scanner(System.in);
-        int n=s.nextInt();
-        for(int i=1 ; i<=n ; i++){
-            if(n%2!=0){
-                for(int j=1 ; j<=n-2 ; j++){
-                    System.out.print(j);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1 ; i<=n ; i++)
+        {
+            if(i==n){
+                for(int j=1 ; j<=n ; j++){
+                    System.out.print("*");
                 }
-                for(int k=1 ; k<n-2 ; k++){
-                    System.out.print(k);
-                }
-                System.out.println();
             }
             else{
-                for(int j=1 ; j<=n-2 ; j++){
-                    System.out.print(j);
+                for(int j=1 ; j<=n ; j++){
+                    if(j==1 || j==i)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
                 }
-                for(int k=1 ; k<n-2 ; k++){
-                    System.out.print(k);
-                }
-                System.out.println();
             }
+            System.out.println();
         }
     }
 }
