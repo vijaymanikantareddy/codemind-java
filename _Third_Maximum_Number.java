ee092c9e9856682d8680a36e1eaafa079a88518c
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-10-29 12:11:58 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int arr[] = new int[n];         TreeSet<Integer> ts = new TreeSet<>();         for(int i=0 ; i<n ; i++){             arr[i] = sc.nextInt();             ts.add(arr[i]);         }         arr = new int[ts.size()];         int ind=0;         for(int ele : ts){             arr[ind++] = ele;         }         if(arr.length>2)             System.out.println(arr[arr.length-3]);         else System.out.println(arr[arr.length-1]);     } }

---
+++ _Third_Maximum_Number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        TreeSet<Integer> ts = new TreeSet<>();
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+            ts.add(arr[i]);
+        }
+        arr = new int[ts.size()];
+        int ind=0;
+        for(int ele : ts){
+            arr[ind++] = ele;
+        }
+        if(arr.length>2)
+            System.out.println(arr[arr.length-3]);
+        else System.out.println(arr[arr.length-1]);
+    }
+}
