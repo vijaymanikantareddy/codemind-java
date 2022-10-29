2e411f396b6a05071d6ae2d71b9b29c9e6195e98
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-10-29 10:23:24 +0530

    import java.util.*;
    import java.io.*;
     
    class Code
    {
        public static void main(String args[])
        {
            double p,t,r;
            Scanner s=new Scanner(System.in);
            p=s.nextDouble();
            r=s.nextDouble();
            t=s.nextDouble(); 
            double c = p *(Math.pow((1 + (r / 100)), t));
            System.out.format("%.2f",c);
        }
    }

--- Compound_interest.java
+++ Compound_interest.java
@@ -1,13 +1,18 @@
+
 import java.util.*;
-class Sample
+
+import java.io.*;
+ 
+class Code
 {
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        double p = sc.nextDouble();
-        double r = sc.nextDouble();
-        int t = sc.nextInt();
-        double res = p * (Math.pow((1 + r / 100), t));
-        System.out.printf("%.2f", res);
+        double p,t,r;
+        Scanner s=new Scanner(System.in);
+        p=s.nextDouble();
+        r=s.nextDouble();
+        t=s.nextDouble(); 
+        double c = p *(Math.pow((1 + (r / 100)), t));
+        System.out.format("%.2f",c);
     }
 }
